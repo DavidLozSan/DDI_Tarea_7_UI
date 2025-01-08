0ed3449617824b1852a2689f2290c5c2acc2f56a
DavidLozSan <davidloz94.dls@gmail.com>
2025-01-08 01:39:14 +0100

    Feature:Vista casi completa.

--- app/src/main/java/edu/iesam/ddi_tarea_7_ui/MainActivity.kt
+++ app/src/main/java/edu/iesam/ddi_tarea_7_ui/MainActivity.kt
@@ -1,7 +1,14 @@
 package edu.iesam.ddi_tarea_7_ui
 
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.viewpager2.widget.ViewPager2
 import edu.iesam.ddi_tarea_7_ui.databinding.ActivityMainBinding
 import edu.iesam.ddi_tarea_7_ui.features.data.local.LocalListOfImages
@@ -9,16 +16,94 @@ import edu.iesam.ddi_tarea_7_ui.features.presentation.CarouselAdapter
 
 class MainActivity : AppCompatActivity() {
 
-    private var _binding: ActivityMainBinding? = null
-    private val binding get() = _binding!!
+    private lateinit var binding: ActivityMainBinding
+    private var favoriteClick = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupView()
 
         val viewPager: ViewPager2 = findViewById(R.id.viewpager)
         val listOfImages = LocalListOfImages().getList()
         viewPager.adapter = CarouselAdapter(listOfImages)
+
+        setupView()
+    }
+
+    private fun setupView() {
+        binding.apply {
+
+            // Botón (Enviar)
+            button2.setOnClickListener {
+                Toast.makeText(this@MainActivity, "Botón (Enviar) pulsado", Toast.LENGTH_SHORT)
+                    .show()
+            }
+
+            // Botón (Hacer una Oferta)
+            offer.setOnClickListener {
+                Toast.makeText(
+                    this@MainActivity,
+                    "Botón (Hacer una Oferta) pulsado",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+
+            // Botón (Comprar)
+            buy.setOnClickListener {
+                Toast.makeText(this@MainActivity, "Botón (Comprar) pulsado", Toast.LENGTH_SHORT)
+                    .show()
+            }
+
+            // Botón (Favorito)
+            favorite.setOnClickListener {
+                if (favoriteClick) {
+                    favoriteClick = false
+                    favorite.text = "18"
+                    favorite.setCompoundDrawablesWithIntrinsicBounds(
+                        R.drawable.baseline_favorite_border_24,
+                        0,
+                        0,
+                        0
+                    )
+                    favorite.compoundDrawableTintList =
+                        ContextCompat.getColorStateList(this@MainActivity, R.color.black)
+                } else {
+                    favoriteClick = true
+                    favorite.text = "19"
+                    favorite.setCompoundDrawablesWithIntrinsicBounds(
+                        R.drawable.baseline_favorite_24,
+                        0,
+                        0,
+                        0
+                    )
+                    favorite.compoundDrawableTintList =
+                        ContextCompat.getColorStateList(this@MainActivity, R.color.red)
+                }
+            }
+
+
+            // Texto (Descripción)
+            val spannableString = SpannableString(getString(R.string.description_text))
+            // Buscar la posición de la palabra "Más"
+            val startIndex = spannableString.toString().indexOf("Más")
+            val endIndex = startIndex + "Más".length
+
+            // Añadir el evento ClickableSpan
+            spannableString.setSpan(object : ClickableSpan() {
+                override fun onClick(widget: View) {
+                    // Mostrar el Toast
+                    Toast.makeText(this@MainActivity, "Botón (Más) pulsado", Toast.LENGTH_SHORT)
+                        .show()
+                }
+            }, startIndex, endIndex, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+            description.text = spannableString
+
+            // Habilitar la detección de clics en el texto
+            description.movementMethod = LinkMovementMethod.getInstance()
+        }
     }
 
 
